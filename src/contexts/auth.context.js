545ecc85b71aa8abc37eed128389636ab0488d91
Asadrayat <asad.rayat4@gmail.com>
2022-10-23 23:28:28 +0600

    private route done

---
+++ src/contexts/auth.context.js
@@ -0,0 +1,12 @@
+const { createContext, useState } = require("react");
+
+export const AuthContext =createContext();
+const AuthProvider = ({children}) =>{
+    const [user,setUser] = useState(null);
+    return (
+       <AuthContext.Provider value={{user,setUser}}>
+           {children}
+       </AuthContext.Provider> 
+    )
+}
+export default AuthProvider
